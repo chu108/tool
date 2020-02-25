446ceb85d92d05e1413c8bbb2c2646994a632aab
chu108 <879656448@qq.com>
2020-02-25 21:59:41 +0800

    init

--- print.go
+++ print.go
@@ -6,10 +6,12 @@ func Err(errs ...interface{}) {
 	for i := 0; i < len(errs); i++ {
 		fmt.Print(fmt.Sprintf("\033[1;32m %v \033[0m", errs[i]))
 	}
+	fmt.Print("\n")
 }
 
 func Info(infos ...interface{}) {
 	for i := 0; i < len(infos); i++ {
 		fmt.Print(fmt.Sprintf("\033[1;32m %v \033[0m", infos[i]))
 	}
+	fmt.Print("\n")
 }

4bc061169d072f566c78f3f2faefb16e896470d6
chu108 <879656448@qq.com>
2020-03-01 21:47:38 +0800

    init

--- print.go
+++ print.go
@@ -2,16 +2,25 @@ package tool
 
 import "fmt"
 
+var (
+	green  = string([]byte{27, 91, 51, 50, 109})
+	yellow = string([]byte{27, 91, 51, 51, 109})
+	red    = string([]byte{27, 91, 51, 49, 109})
+	reset  = string([]byte{27, 91, 48, 109})
+)
+
 func Err(errs ...interface{}) {
 	for i := 0; i < len(errs); i++ {
-		fmt.Print(fmt.Sprintf("\033[1;31m %v \033[0m", errs[i]))
+		fmt.Print(red, errs[i], reset)
+		//fmt.Print(fmt.Sprintf("\033[1;31m %v \033[0m", errs[i]))
 	}
 	fmt.Print("\n")
 }
 
 func Info(infos ...interface{}) {
 	for i := 0; i < len(infos); i++ {
-		fmt.Print(fmt.Sprintf("\033[1;32m %v \033[0m", infos[i]))
+		fmt.Print(green, infos[i], reset)
+		//fmt.Print(fmt.Sprintf("\033[1;32m %v \033[0m", infos[i]))
 	}
 	fmt.Print("\n")
 }

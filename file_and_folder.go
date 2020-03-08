f380515cd19a77a382180bee63d3b3312264fed1
chu108 <879656448@qq.com>
2020-03-08 19:04:43 +0800

    init

--- file_and_folder.go
+++ file_and_folder.go
@@ -2,13 +2,8 @@ package tool
 
 import (
 	"bufio"
-	"bytes"
-	"errors"
 	"io"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"path"
 	"path/filepath"
 )
 
@@ -106,50 +101,4 @@ func ReadDirFiles(dir string) ([]string, error) {
 */
 func ReadDirFilesAll() {
 
-}
-
-/**
-下载文件
-url：下载地址
-savePath：保存路径，不包含文件名
-saveName：保存文件名，如果为空，则用原名
-*/
-func DownloadFile(url, savePath, saveName string) error {
-	if url == "" || savePath == "" {
-		return errors.New("下载url或保存地址错误")
-	}
-	//获取文件类型
-	fileExt := path.Ext(url)
-	//获取文件名
-	fileName := path.Base(url)
-	if saveName == "" {
-		saveName = fileName
-	}
-
-	if savePath[len(savePath)-1:] != "/" {
-		savePath += "/"
-	}
-
-	res, err := http.Get(url)
-	if err != nil || res.StatusCode != 200 {
-		return err
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-
-	file, err := os.Create(savePath + saveName + fileExt)
-	if err != nil {
-		return err
-	}
-
-	writerLen, err := io.Copy(file, bytes.NewReader(body))
-	if err != nil {
-		return err
-	}
-	Info("下载总长度：", writerLen)
-	return nil
 }

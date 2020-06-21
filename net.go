2802df3a400f4b3c93837bbd00f2ffdfe8b1c379
chu108 <879656448@qq.com>
2020-06-21 14:13:38 +0800

    1

--- net.go
+++ net.go
@@ -28,9 +28,9 @@ url：下载地址
 savePath：保存路径，不包含文件名
 saveName：保存文件名，如果为空，则用原名
 */
-func DownloadFile(url, savePath, saveName string) error {
+func DownloadFile(url, savePath, saveName string) (string, error) {
 	if url == "" || savePath == "" {
-		return errors.New("下载url或保存地址错误")
+		return "", errors.New("下载url或保存地址错误")
 	}
 	//获取文件类型
 	fileExt := path.Ext(url)
@@ -47,29 +47,29 @@ func DownloadFile(url, savePath, saveName string) error {
 	filePath := savePath + saveName + fileExt
 	if IsExist(filePath) && GetFileSize(filePath) > 0 {
 		Err(saveName, "文件已存在")
-		return nil
+		return "", nil
 	}
 
 	body, err := GetByteForUrl(url)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	file, err := os.Create(filePath)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	writerLen, err := io.Copy(file, bytes.NewReader(body))
 	if err != nil {
-		return err
+		return "", err
 	}
 	Info("下载总长度：", writerLen)
 	if writerLen == 0 {
 		os.Remove(filePath)
 	}
 
-	return nil
+	return filePath, nil
 }
 
 /**

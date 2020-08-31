5fe63d1e5ee7101b42fd8e0c4f39e1c6693e0208
褚志勇 <zhiyong_chu@qingsongchou.com>
2020-08-31 15:48:55 +0800

    添加打开网页函数

---
+++ browser.go
@@ -0,0 +1,19 @@
+package tool
+
+import (
+	"os/exec"
+	"runtime"
+)
+
+//打开网页
+func OpenBrowser(url string) (err error) {
+	switch runtime.GOOS {
+	case "windows": //windows
+		err = exec.Command(`cmd`, `/c`, `start`, url).Start()
+	case "darwin": //Linux
+		err = exec.Command(`xdg-open`, url).Start()
+	default: //Mac
+		err = exec.Command(`open`, url).Start()
+	}
+	return
+}
